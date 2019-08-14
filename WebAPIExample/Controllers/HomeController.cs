65b8808aa683d26f1b43f8f8a0f6bab5c13f7140
tavisca-vsharma <vsharma@tavisca.in>
2019-08-14 17:02:23 +0530

    Add project files.

---
+++ WebAPIExample/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIExample.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public string Greeting(string str)
+        {
+            if(str.Equals("hello"))
+            {
+                return "hi I am vibhu";
+               
+            }
+            else
+            {
+                return "invalid";
+            }
+        }
+    }
+}
